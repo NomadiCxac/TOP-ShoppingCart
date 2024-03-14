0050d2ac70626f70b8bfd87dacee9dbcdb9f1a32
NomadiCxac <christianxavier.cordero@gmail.com>
2024-03-14 12:14:12 -0400

    refactor sign out from button to function

---
+++ shoppingCart/src/functions/signOutUser.js
@@ -0,0 +1,30 @@
+export const signOutUser = async (user, userSignOut, setReferenceOrderId, navigate) => {
+
+    setReferenceOrderId(null);
+    if (localStorage.getItem('anonymousOrderId')) {
+        localStorage.removeItem('anonymousOrderId');
+    }
+
+    if (user) {
+        if (user.isAnonymous) {
+            // If the user is anonymous, delete the user before signing out
+            try {
+                await user.delete();
+                console.log('Anonymous user deleted');
+            } catch (error) {
+                console.error('Error deleting anonymous user:', error);
+            }
+        }
+
+        // For non-anonymous users or after deleting an anonymous user, call the sign out function
+        try {
+            await userSignOut();
+            console.log('User signed out');
+        } catch (error) {
+            console.error('Error signing out:', error);
+        }
+    }
+
+    // Execute navigation after sign out
+    navigate('/orderManagement');
+};
