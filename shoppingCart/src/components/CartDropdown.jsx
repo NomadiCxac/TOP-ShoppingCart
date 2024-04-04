87884082832696b3990cd564229e9569b2e6cb28
NomadiCxac <christianxavier.cordero@gmail.com>
2024-04-04 10:16:21 -0400

    updated hosting data files

--- shoppingCart/src/components/CartDropdown.jsx
+++ shoppingCart/src/components/CartDropdown.jsx
@@ -1,28 +1,27 @@
 import './CartDropdown.css'
 
-// const CartDropdown = ({ isVisible, item, onClose, imageURL, className }) => {
-//     if (!isVisible) return null;
+const CartDropdown = ({ isVisible, item, onClose, imageURL, className }) => {
+    if (!isVisible) return null;
 
+    return (
+        <div className={className}>
+             <img className="cartDropdownImage" src={imageURL}
+                         onError={(e) => e.currentTarget.src = '/images/defaultFood.jpeg'} 
+                        //  alt={item.name} 
+                    />
+            <div className="cartItemDropdownDetails">
+                {item.quantity} {item.name} has been added to your cart.
+            </div>
+            <p 
+                className="close-button" 
+                id="dropdown" 
+                aria-label="Close modal"
+                onClick={onClose}
+            >
+                &times;
+            </p>
+        </div>
+    );
+};
 
-//     return (
-//         <div className={className}>
-//              <img className="cartDropdownImage" src={imageURL}
-//                          onError={(e) => e.currentTarget.src = '/images/defaultFood.jpeg'} 
-//                         //  alt={item.name} 
-//                     />
-//             <div className="cartItemDropdownDetails">
-//                 {item.quantity} {item.name} has been added to your cart.
-//             </div>
-//             <p 
-//                 className="close-button" 
-//                 id="dropdown" 
-//                 aria-label="Close modal"
-//                 onClick={onClose}
-//             >
-//                 &times;
-//             </p>
-//         </div>
-//     );
-// };
-
-// export default CartDropdown;
+export default CartDropdown;
